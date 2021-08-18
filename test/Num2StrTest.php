19442ee3a61b4252141444ca104118bd574263e0
slava <vyacheslav.voloshyn@coolcredit.cz>
2021-08-18 16:12:26 +0200

    Add testes

--- test/Num2StrTest.php
+++ test/Num2StrTest.php
@@ -13,6 +13,9 @@ class Num2StrTest extends TestCase
     private const WRONG_VALUES = 'wrongValues';
     private const CORRECT_VALUES = 'correctValues';
 
+    private const RU_LOCALE = 'ru';
+    private const UA_LOCALE = 'ua';
+
     private const TEST_CASES = [
         1 => [
             self::WRONG_VALUES => [
@@ -94,14 +97,168 @@ class Num2StrTest extends TestCase
         ],
     ];
 
+    private const TEST_CASES_WITHOUT_UNITS = [
+        2 => [
+            self::WRONG_VALUES => [
+                'Одна гривня 0 копійок',
+                'Одна гривна 00 копійка',
+                '1 гривня 00 копійка',
+            ],
+            self::CORRECT_VALUES => 'два'
+        ],
+        1 => [
+            self::WRONG_VALUES => [
+                'Одна гривня 0 копійок',
+                'Одна гривна 00 копійка',
+                '1 гривня 00 копійка',
+            ],
+            self::CORRECT_VALUES => 'один'
+        ],
+        31 => [
+            self::WRONG_VALUES => [
+                'Одна гривня 0 копійок',
+                'Одна гривна 00 копійка',
+                '1 гривня 00 копійка',
+            ],
+            self::CORRECT_VALUES => 'тридцять один'
+        ]
+    ];
+
+    private const PLURAL_TEST = [
+        1 => [
+            self::UA_LOCALE => [
+                self::WRONG_VALUES => [
+                    'дня',
+                    'днів',
+                ],
+                self::CORRECT_VALUES => 'день',
+            ],
+            self::RU_LOCALE => [
+                self::WRONG_VALUES => [
+                    'дней',
+                    'дня',
+                ],
+                self::CORRECT_VALUES => 'день',
+            ],
+        ],
+        15 => [
+            self::UA_LOCALE => [
+                self::WRONG_VALUES => [
+                    'дня',
+                    'день',
+                ],
+                self::CORRECT_VALUES => 'днів',
+            ],
+            self::RU_LOCALE => [
+                self::WRONG_VALUES => [
+                    'день',
+                    'дня',
+                ],
+                self::CORRECT_VALUES => 'дней',
+            ],
+        ],
+        2 => [
+            self::UA_LOCALE => [
+                self::WRONG_VALUES => [
+                    'день',
+                    'днів',
+                ],
+                self::CORRECT_VALUES => 'дня',
+            ],
+            self::RU_LOCALE => [
+                self::WRONG_VALUES => [
+                    'день',
+                    'дней',
+                ],
+                self::CORRECT_VALUES => 'дня',
+            ],
+        ],
+    ];
+
+    private const PLURAL_TEST_NUMBER_STRING = [
+        1 => [
+            self::UA_LOCALE => [
+                self::WRONG_VALUES => [
+                    'дня',
+                    'днів',
+                ],
+                self::CORRECT_VALUES => 'один день',
+            ],
+            self::RU_LOCALE => [
+                self::WRONG_VALUES => [
+                    'дней',
+                    'дня',
+                ],
+                self::CORRECT_VALUES => 'один день',
+            ],
+        ],
+        15 => [
+            self::UA_LOCALE => [
+                self::WRONG_VALUES => [
+                    'дня',
+                    'день',
+                ],
+                self::CORRECT_VALUES => 'п\'ятнадцять днів',
+            ],
+            self::RU_LOCALE => [
+                self::WRONG_VALUES => [
+                    'день',
+                    'дня',
+                ],
+                self::CORRECT_VALUES => 'пятнадцать дней',
+            ],
+        ],
+        2 => [
+            self::UA_LOCALE => [
+                self::WRONG_VALUES => [
+                    'день',
+                    'днів',
+                ],
+                self::CORRECT_VALUES => 'два дня',
+            ],
+            self::RU_LOCALE => [
+                self::WRONG_VALUES => [
+                    'день',
+                    'дней',
+                ],
+                self::CORRECT_VALUES => 'два дня',
+            ],
+        ],
+    ];
+
     public function testNum2StrConverting()
     {
         foreach (self::TEST_CASES as $number => $results) {
-            $formattedNumber = Num2Str::convert($number);
+            $formattedNumber = \CoolCredit\Libs\Utils\Num2Str::convert($number);
             foreach ($results[self::WRONG_VALUES] as $wrongValue) {
                 $this->assertNotEquals($formattedNumber, $wrongValue);
             }
             $this->assertEquals($formattedNumber, $results[self::CORRECT_VALUES]);
         }
+    }
+
+    public function testNum2StrConvertingWithoutUnits()
+    {
+        foreach (self::TEST_CASES_WITHOUT_UNITS as $number => $results) {
+            $formattedNumber = Num2Str::convertWithoutUnits($number);
+            foreach ($results[self::WRONG_VALUES] as $wrongValue) {
+                $this->assertNotEquals($formattedNumber, $wrongValue);
+            }
+            $this->assertEquals($formattedNumber, $results[self::CORRECT_VALUES]);
+        }
+    }
+
+    public function testPluralForm()
+    {
+        foreach (self::PLURAL_TEST_NUMBER_STRING as $number => $locales) {
+            foreach ($locales as $locale => $testCases) {
+                $formattedNumber = Num2Str::pluralFormWithConvertedString($number, $locale);
+                foreach ($testCases[self::WRONG_VALUES] as $wrongValue) {
+                    $this->assertNotEquals($formattedNumber, $wrongValue);
+                }
+                $this->assertEquals($formattedNumber, $testCases[self::CORRECT_VALUES]);
+            }
+        }
+        var_dump(Num2Str::pluralFormWithConvertedString(10));
     }
 }
